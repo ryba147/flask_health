fa70d899ed1dc154d2c41142fc04bc32a9d9e2ba
Taras Vilinskyi <kot3myp3uk@gmail.com>
2021-12-31 16:23:01 +0200

    Before/after request event logging

--- flask_health/routes.py
+++ flask_health/routes.py
@@ -1,16 +1,31 @@
-from flask import Blueprint, jsonify
+import time
+from datetime import timedelta
 
-"""
-    health check
-    
-    from config flask [+-]
-    
-    models with schemas
-    
-    views with logic
-"""
+from flask import Blueprint, jsonify, g, request
+
+from flask_health.logging import logger
 
 common_bp = Blueprint('common_bp', __name__)
+
+
+# 1. Separate for each Blueprint ?
+# 2. Log the name of a Blueprint request was made from ?
+# 3. Import created object or make a logger factory ?
+#  3.1. def createLogger(name): return getLogger(name) ? singleton ?
+@common_bp.before_app_request
+def before_app_request():
+    g.REQUEST_START_TIME = time.perf_counter()
+
+
+@common_bp.after_app_request
+def after_app_request(response):
+    elapsed_time = time.perf_counter() - g.REQUEST_START_TIME
+
+    logger.info('"{} {}?{}" finished with status code {}. Time elapsed: {}'.format(request.method, request.path,
+                                                                                   request.query_string.decode('utf-8'),
+                                                                                   response.status_code,
+                                                                                   timedelta(seconds=elapsed_time)))
+    return response
 
 
 @common_bp.route('/')
